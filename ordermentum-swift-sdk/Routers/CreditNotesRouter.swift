6445c88fd87bf5bad2fe52bc8e0241284e142a6a
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-04-04 14:33:01 +1100

    Finish CreditNotesService

--- ordermentum-swift-sdk/Routers/CreditNotesRouter.swift
+++ ordermentum-swift-sdk/Routers/CreditNotesRouter.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import Alamofire
+
+public enum CreditNotesRouter: URLRequestConvertible {
+    //Routes
+    case getCreditNotes(String, Int, Int)
+    
+    //Methods
+    var method: HTTPMethod {
+        switch self {
+        case .getCreditNotes:
+            return .get
+        }
+    }
+    
+    //Paths
+    var path: String {
+        switch self {
+        case .getCreditNotes:
+            return "credit-notes"
+        }
+    }
+    
+    //Parameters
+    var parameters: [String: Any] {
+        switch self {
+        case .getCreditNotes(let purchaserId, let pageSize, let pageNo):
+            return ["purchaserId": purchaserId, "pageSize": pageSize, "pageNo": pageNo]
+        default:
+            return [:]
+        }
+    }
+    
+    //Body
+    var body: Codable? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+    
+    //Builder
+    public func asURLRequest() throws -> URLRequest {
+        //Setup Data
+        let url = try Client.instance.baseURL.asURL()
+        let timeoutSeconds: Int = 10
+        
+        //Build Request
+        var request = URLRequest(url: url.appendingPathComponent(path))
+        request.httpMethod = method.rawValue
+        request.setValue(Client.instance.getHeaderToken(), forHTTPHeaderField: "Authorization")
+        request.timeoutInterval = TimeInterval(timeoutSeconds * 1000)
+        
+        //Set Conditional Body
+        if body != nil {
+            request.httpBody = body?.toJSONData()
+        }
+        
+        return try URLEncoding.default.encode(request, with: parameters)
+    }
+}
--- ordermentum-swift-sdk/Services/CreditNotesService.swift
+++ ordermentum-swift-sdk/Services/CreditNotesService.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import Alamofire
+
+class CreditNotesService {
+    /**
+     * Get the credit notes belonging to a purchaser
+     * Returns a CreditNotesResponse
+     */
+    func getCreditNotes(purchaserId: String, pageSize: Int, pageNo: Int, completion: @escaping (Bool, CreditNotesResponse?) -> ()) {
+        //Build Route
+        let route = CreditNotesRouter.getCreditNotes(purchaserId, pageSize, pageNo) as URLRequestConvertible
+        
+        //Call API
+        Service<CreditNotesResponse>().request(route: route) { (result, responseObject) in
+            completion(result, responseObject)
+        }
+    }
+}
