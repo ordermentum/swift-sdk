bec90a6ac528fc5600eee8886db91f0cfc367c0b
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-04-08 22:01:07 +1000

    NotifyService

---
+++ ordermentum-swift-sdk/Services/NotifyService.swift
@@ -0,0 +1,27 @@
+//
+//  NotifyService.swift
+//  ordermentum-swift-sdk
+//
+//  Created by Brandon Stillitano on 8/4/19.
+//  Copyright © 2019 Ordermentum. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NotifyService {
+    /**
+     * Send device details for a user to the Ordermentum service
+     * Returns a ResponseBody which can be used to check for a 200 response
+     */
+    func registerDevice(requestObject: NotifyBody, completion: @escaping (Bool) -> ()) {
+        //Build Route
+        let route = NotifyRouter.registerDevice(requestObject) as URLRequestConvertible
+        
+        //Call API
+        Service<FindSupplierResponse>().request(route: route) { (result, responseObject) in
+            completion(result)
+        }
+    }
+}
+
