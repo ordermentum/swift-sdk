ddebec450b5b97bf3e276ccca604004fb5ff7066
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-04-08 17:02:47 +1000

    NotificationsService

---
+++ ordermentum-swift-sdk/Services/NotificationsService.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationsService.swift
+//  ordermentum-swift-sdk
+//
+//  Created by Brandon Stillitano on 8/4/19.
+//  Copyright © 2019 Ordermentum. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NotificationsService {
+    /**
+     * Update a particular notification setting for a supplier to retailer relationship
+     * Returns a ResponseBody
+     */
+    func updateNotificationSetting(userId: String, supplierId: String, updateObject: [String: Bool], completion: @escaping (Bool) -> ()) {
+        //Build Route
+        let route = NotificationsRouter.updateNotificationSetting(userId, supplierId, updateObject) as URLRequestConvertible
+        
+        //Call API
+        Service<EmptyDecodable>().request(route: route) { (result, responseObject) in
+            completion(result)
+        }
+    }
+}
