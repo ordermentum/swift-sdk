af1938f80c35c1fcd5748ff4f565bb3806646f4d
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-04-08 16:56:26 +1000

    MarketplaceService

---
+++ ordermentum-swift-sdk/Services/MarketplaceService.swift
@@ -0,0 +1,27 @@
+//
+//  MarketplaceService.swift
+//  ordermentum-swift-sdk
+//
+//  Created by Brandon Stillitano on 8/4/19.
+//  Copyright © 2019 Ordermentum. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class MarketplaceService {
+    /**
+     * Fetch the marketplaces for a selected retailer
+     * Returns a Marketplace object
+     */
+    func getMarketplaces(retailerId: String, pageSize: Int, pageNo: Int, completion: @escaping (Bool, MarketplaceResponse?) -> ()) {
+        //Build Route
+        let route = MarketplaceRouter.getMarketplaces(retailerId, pageSize, pageNo) as URLRequestConvertible
+        
+        //Call API
+        Service<MarketplaceResponse>().request(route: route) { (result, responseObject) in
+            completion(result, responseObject)
+        }
+    }
+}
+
