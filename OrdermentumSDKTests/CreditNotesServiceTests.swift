247d9acdedaf21a2941ad984447ac229ed696f90
Mark Kenneth Bayona <mark.bayona@ordermentum.com>
2019-04-15 14:20:42 +0800

    CreditNotesService tests

---
+++ OrdermentumSDKTests/CreditNotesServiceTests.swift
@@ -0,0 +1,37 @@
+//
+//  CreditNotesServiceTests.swift
+//  OrdermentumSDKTests
+//
+//  Created by Mark Kenneth Bayona on 15/04/2019.
+//  Copyright © 2019 Ordermentum. All rights reserved.
+//
+
+import XCTest
+@testable import OrdermentumSDK
+
+class CreditNotesServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGetCreditNotes() {
+        //Build Expectation
+        let expectation = XCTestExpectation(description: "Async Test")
+        
+        //Call API
+        Client.instance.setProductionURL()
+        CreditNotesService().getCreditNotes(purchaserId: "", pageSize:0, pageNo:0) { (result, responseData) in
+            assert(result)
+            expectation.fulfill()
+        }
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+}
