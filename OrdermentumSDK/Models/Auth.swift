a377ee131e8a36bfd0cec5df7647ceae5766840d
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-04-16 10:44:31 +1000

    Add public inits (testing before complete implementation)

--- OrdermentumSDK/Models/Auth.swift
+++ OrdermentumSDK/Models/Auth.swift
@@ -14,20 +14,28 @@ public struct Login: Codable {
 }
 
 public struct LoginRequest: Codable {
+    public init() {}
+    
     public var username: String = ""
     public var password: String = ""
 }
 
 public struct ForgotPasswordRequest: Codable {
+    public init() {}
+    
     public var email: String = ""
 }
 
 public struct ChangePassword: Codable {
+    public init() {}
+    
     public var success: Bool = false
     public var message: String = ""
 }
 
 public struct ChangePasswordRequest: Codable {
+    public init() {}
+    
     public var userId: String = ""
     public var oldPassword: String = ""
     public var password: String = ""
