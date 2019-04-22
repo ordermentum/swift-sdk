95d3c15dc66ad415703b341df482c5d749bc01a3
Mark Kenneth Bayona <mark.bayona@ordermentum.com>
2019-04-22 10:48:36 +0800

    Added test for SubmitOrder()

--- OrdermentumSDKTests/Helpers/NetworkStub.swift
+++ OrdermentumSDKTests/Helpers/NetworkStub.swift
@@ -12,6 +12,7 @@ import Hippolyte
 public enum StubDataFile: String {
     case AddonsSearch
     case AddonsSearchError = "jsonError"
+    case OrderSubmit
 }
 
 protocol NetworkStubs {
@@ -53,3 +54,4 @@ extension NetworkStubs {
 
 extension AddOnsServiceTests : NetworkStubs {}
 extension NotifyServiceTests : NetworkStubs {}
+extension OrderServiceTests : NetworkStubs {}
--- OrdermentumSDKTests/OrdersServiceTests.swift
+++ OrdermentumSDKTests/OrdersServiceTests.swift
@@ -20,6 +20,53 @@ class OrderServiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testSubmitOrder() {
+        //Build Expectation
+        let expectation = XCTestExpectation(description: "Async Test")
+        
+        //Build lineItems array for CreateOrder object
+        var lineItems: [CreateOrderLineItem] = []
+        let lineItemsJSONString = ProcessInfo.processInfo.environment["CREATE_ORDER_LINE_ITEMS"] ?? ""
+        if !lineItemsJSONString.isEmpty {
+            let lineItemsJSONData = lineItemsJSONString.data(using: .utf8) ?? Data()
+            if let json = try? JSONSerialization.jsonObject(with: lineItemsJSONData), let array = json as? [CreateOrderLineItem] {
+                lineItems = array
+            }
+        }
+
+        //Build request body and params
+        var requestObject:CreateOrder = CreateOrder()
+        requestObject.lineItems = lineItems
+        requestObject.comment = ProcessInfo.processInfo.environment["CREATE_ORDER_COMMENT"] ?? ""
+        requestObject.deliveryDate = ProcessInfo.processInfo.environment["CREATE_ORDER_DELIVERY_DATE"] ?? ""
+        requestObject.origin = ProcessInfo.processInfo.environment["CREATE_ORDER_ORIGIN"] ?? ""
+        requestObject.reference = ProcessInfo.processInfo.environment["CREATE_ORDER_REFERENCE"] ?? ""
+        requestObject.retailerId = ProcessInfo.processInfo.environment["CREATE_ORDER_RETAILER_ID"] ?? ""
+        requestObject.status = ProcessInfo.processInfo.environment["CREATE_ORDER_STATUS"] ?? ""
+        requestObject.supplierId = ProcessInfo.processInfo.environment["CREATE_ORDER_SUPPLIER_ID"] ?? ""
+        requestObject.tokenId = ProcessInfo.processInfo.environment["CREATE_ORDER_TOKEN_ID"] ?? ""
+        requestObject.type = ProcessInfo.processInfo.environment["CREATE_ORDER_TYPE"] ?? ""
+        requestObject.paymentMethodId = ProcessInfo.processInfo.environment["CREATE_ORDER_PAYMENT_METHOD_ID"] ?? ""
+
+        //Request setup
+        Client.instance.baseURL = ClientURL.rootTestingURL
+        Client.instance.token = ProcessInfo.processInfo.environment["ACCESS_TOKEN"] ?? ""
+        
+        //Stubbing
+        if let route = try? OrdersRouter.submitOrder(requestObject).asURLRequest() {
+            self.startStub(route, stubData: .OrderSubmit )
+        }
+        
+        //Call API
+        Client.instance.orders.submitOrder(requestObject) { (result, responseData) in
+            assert(result)
+            expectation.fulfill()
+        }
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
     func testGetDeliveryDates() {
         //Build Expectation
         let expectation = XCTestExpectation(description: "Async Test")
