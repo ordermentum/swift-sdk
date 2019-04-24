5f2d8af7c7f0cb0202537f4e369e14b52bfe8628
Joseph <joseph.fabian@ordermentum.com>
2019-04-24 11:03:54 +0800

    Resolve Change Request
    Changelog:
    
    1. Resolve Change Request in PR on https://github.com/ordermentum/swift-sdk/pull/5
    
    2. Fixed Conflicts

--- OrdermentumSDK/Models/Marketplace.swift
+++ OrdermentumSDK/Models/Marketplace.swift
@@ -78,7 +78,7 @@ extension Marketplace: Decodable {
         brandImages = try container.decodeIfPresent([Image].self, forKey: .brandImages) ?? []
         logo = try container.decodeIfPresent(Image.self, forKey: .logo) ?? Image()
         settings = try container.decodeIfPresent(MarketplaceSettings.self, forKey: .settings) ?? MarketplaceSettings()
-//        properties = try container.decodeIfPresent(Properties.self, forKey: .properties) ?? Properties()
+        properties = try container.decodeIfPresent(Properties.self, forKey: .properties) ?? Properties()
         shortName = try container.decodeIfPresent(String.self, forKey: .shortName) ?? ""
         phone = try container.decodeIfPresent(String.self, forKey: .phone) ?? ""
         activeRetailerIds = try container.decodeIfPresent([String].self, forKey: .activeRetailerIds) ?? []
@@ -101,6 +101,7 @@ extension Properties: Decodable {
         demoSupplier = try container.decodeIfPresent(String.self, forKey: .demoSupplier) ?? ""
     }
 }
+
 
 extension MarketplaceSettings: Decodable {
     public init(from decoder: Decoder) throws {
