70da2ea8fc83091ba4cdba56c6e14d4a295d895f
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-05-06 12:10:53 +1000

    Add Signup function

--- OrdermentumSDK/Services/AuthService.swift
+++ OrdermentumSDK/Services/AuthService.swift
@@ -66,4 +66,18 @@ public class AuthService {
             completion(result)
         }
     }
+    
+    /**
+     * Registers a user account on the Ordermentum retailer platform.
+     * Returns a SignUpResponse
+     */
+    public func registerUser(requestObject: SignUpUser, completion: @escaping (Bool) -> ()) {
+        //Build Route
+        let route = AuthRouter.registerUser(requestObject) as URLRequestConvertible
+        
+        //Call API
+        Service<EmptyDecodable>().request(route: route) { (result, responseObject) in
+            completion(result)
+        }
+    }
 }
