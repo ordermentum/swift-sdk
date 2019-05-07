b6f6608e297a41b4228a23ddf251df49d95b1563
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-05-07 11:05:07 +1000

    Add extension to order

--- OrdermentumSDK/Models/Order.swift
+++ OrdermentumSDK/Models/Order.swift
@@ -543,3 +543,28 @@ extension LineItem: Equatable {
         return lhs.product.id == rhs.product.id
     }
 }
+
+extension Order {
+    public func toCreateFavouriteRequest(retailerId: String, supplierId: String) -> CreateFavouriteRequest {
+        //Initialize Create Favourite Request
+        var requestObject: CreateFavouriteRequest = CreateFavouriteRequest()
+        
+        //Add Line Items
+        for lineItem in self.lineItems {
+            //Create Line item
+            var createFavouriteLineItem: CreateFavouriteRequestLineItem = CreateFavouriteRequestLineItem()
+            createFavouriteLineItem.name = lineItem.product.name
+            createFavouriteLineItem.price = Float(lineItem.product.formattedPrice) ?? 0
+            createFavouriteLineItem.productId = lineItem.product.id
+            createFavouriteLineItem.quantity = lineItem.quantity
+            requestObject.lineItems.append(createFavouriteLineItem)
+        }
+        
+        //Set Favourite Object
+        requestObject.retailerId = retailerId
+        requestObject.supplierId = supplierId
+        requestObject.type = "template"
+        
+        return requestObject
+    }
+}
