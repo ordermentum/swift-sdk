f7b84de8e94b6ffa186c3f8b4444cb08a7701fe5
Mark Kenneth Bayona <mark.bayona@ordermentum.com>
2019-06-04 11:23:30 +0800

    Adjustments to AppVersion and AppVersionService

--- OrdermentumSDK/Models/AppVersion.swift
+++ OrdermentumSDK/Models/AppVersion.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+public struct AppVersion: Decodable {
+    
+    public var minimumAppVersion: String?
+    enum CodingKeys: String, CodingKey {
+        case minimumAppVersion = "minimum-version"
+    }
+    
+    public init(minimumAppVersion: String?) {
+        self.minimumAppVersion = minimumAppVersion
+    }
+    public static func == (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
+            return false
+        }
+        return version1.compare(version2, options: .numeric) == .orderedSame
+    }
+    
+    public static func < (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
+            return false
+        }
+        return version1.compare(version2, options: .numeric) == .orderedAscending
+    }
+    
+    public static func <= (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
+            return false
+        }
+        return version1.compare(version2, options: .numeric) == .orderedAscending || version1.compare(version2, options: .numeric) == .orderedSame
+    }
+    
+    public static func > (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
+            return false
+        }
+        return version1.compare(version2, options: .numeric) == .orderedDescending
+    }
+    
+    public static func >= (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
+            return false
+        }
+        return version1.compare(version2, options: .numeric) == .orderedDescending || version1.compare(version2, options: .numeric) == .orderedSame
+    }
+    
+}
--- OrdermentumSDK/Services/AppVersionService.swift
+++ OrdermentumSDK/Services/AppVersionService.swift
@@ -6,69 +6,25 @@
 //  Copyright © 2018 Ordermentum Pty Ltd. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import Alamofire
 
-struct AppVersion: Decodable {
-    var minimumAppVersion: String?
-    enum CodingKeys: String, CodingKey {
-        case minimumAppVersion = "minimum-version"
-    }
-
-    static func == (lhs: OMAppVersion, rhs: OMAppVersion) -> Bool {
-        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
-            return false
-        }
-        return version1.compare(version2, options: .numeric) == .orderedSame
-    }
-
-    static func < (lhs: OMAppVersion, rhs: OMAppVersion) -> Bool {
-        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
-            return false
-        }
-        return version1.compare(version2, options: .numeric) == .orderedAscending
-    }
-
-    static func <= (lhs: OMAppVersion, rhs: OMAppVersion) -> Bool {
-        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
-            return false
-        }
-        return version1.compare(version2, options: .numeric) == .orderedAscending || version1.compare(version2, options: .numeric) == .orderedSame
-    }
-
-    static func > (lhs: OMAppVersion, rhs: OMAppVersion) -> Bool {
-        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
-            return false
-        }
-        return version1.compare(version2, options: .numeric) == .orderedDescending
-    }
-
-    static func >= (lhs: OMAppVersion, rhs: OMAppVersion) -> Bool {
-        guard let version1 = lhs.minimumAppVersion, let version2 = rhs.minimumAppVersion else {
-            return false
-        }
-        return version1.compare(version2, options: .numeric) == .orderedDescending || version1.compare(version2, options: .numeric) == .orderedSame
-    }
-
-}
-
-class AppVersionService <ExpectedResponse: Decodable> {
-
-    func checkVersion(completion: @escaping APICompletionHandler<ExpectedResponse>) {
-        let codableService = CodableService<ExpectedResponse>()
-        let urlString = "https://static.ordermentum.com/ios/manifest.json"
-        codableService.defaultRequest(urlString: urlString) { (appVersion, _, error) in
-            var apiResponse = APIResponse<ExpectedResponse>(apiStatus: APIStatus.success, data: appVersion, error: error)
-            guard error == nil else {
-                apiResponse.apiStatus = .error
-                completion(apiResponse)
-                return
+public class AppVersionService {
+    public init() {}
+    public func checkVersion(completion: @escaping (Bool, AppVersion? ) -> ()) {
+        //Build Route
+        do{
+            let route = try Client.instance.urlRequest(path: "https://static.ordermentum.com/ios/manifest.json", method: .get, parameters: [:], body: nil) as URLRequestConvertible
+            
+            //Call API
+            Service<AppVersion>().request(route: route) { (result, responseObject) in
+                completion(result, responseObject)
             }
-            guard appVersion.decoded != nil else {
-                apiResponse.apiStatus = .failed
-                completion(apiResponse)
-                return
-            }
-            completion(apiResponse)
         }
+        catch
+        {
+            print("Error: \(error)")
+        }
+        
     }
 }
