14f3891065e80d6f4a39eda16a287dfbce221fac
Brandon Stillitano <brandon.stillitano@ordermentum.com>
2019-08-06 18:58:48 +1000

    Dynamic timeouts

--- OrdermentumSDK/Routers/OrdersRouter.swift
+++ OrdermentumSDK/Routers/OrdersRouter.swift
@@ -122,8 +122,18 @@ public enum OrdersRouter: URLRequestConvertible {
         }
     }
     
+    //Timeout
+    var timeout: Int {
+        switch self {
+        case .submitOrder:
+            return 120
+        default:
+            return 10
+        }
+    }
+    
     //Builder
     public func asURLRequest() throws -> URLRequest {
-        return try Client.instance.urlRequest(path: path, method: method, parameters: parameters, body: body)
+        return try Client.instance.urlRequest(path: path, method: method, parameters: parameters, body: body, timeout: timeout)
     }
 }
