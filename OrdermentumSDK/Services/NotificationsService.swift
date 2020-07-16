b10c4a089d7838f3f182123d592230ebcfbdf181
Joseph Fabian <jfabianzarsuela@gmail.com>
2020-07-16 11:35:46 +0800

    Fixed Lint issues

--- OrdermentumSDK/Services/NotificationsService.swift
+++ OrdermentumSDK/Services/NotificationsService.swift
@@ -40,7 +40,7 @@ public class NotificationsService {
         }
     }
 
-    public func getNotification(userId: String, completion: @escaping (Bool, VenueNotificationsResponse?, ErrorResponse?) -> ()) {
+    public func getNotification(userId: String, completion: @escaping (Bool, VenueNotificationsResponse?, ErrorResponse?) -> Void) {
         //Build Route
         let route = VenueNotificationsRouter.getNotifications(userId) as URLRequestConvertible
 
@@ -50,12 +50,12 @@ public class NotificationsService {
         }
     }
 
-    public func readNotification(notificationId: String, completion: @escaping (Bool, ErrorResponse?) -> ()) {
+    public func readNotification(notificationId: String, completion: @escaping (Bool, ErrorResponse?) -> Void) {
         //Build Route
         let route = VenueNotificationsRouter.markNotificationAsRead(notificationId) as URLRequestConvertible
 
         //Call API
-        Service<EmptyDecodable, ErrorResponse>().request(route: route) { (result, responseObject, errorObject) in
+        Service<EmptyDecodable, ErrorResponse>().request(route: route) { (result, _, errorObject) in
             completion(result, errorObject)
         }
     }
