0ec129a8998e17734096dbb467dfd54c2ea40e6d
Brandon Stillitano <b.stillitano95@gmail.com>
2021-01-05 12:19:24 +1100

    Fix lint

--- OrdermentumSDK/Services/CartService.swift
+++ OrdermentumSDK/Services/CartService.swift
@@ -10,8 +10,8 @@ import Foundation
 import Alamofire
 
 public class CartService {
-    public init() {}
-    
+    public init() { }
+
     /**
      * Checks the current logged in user against the given `cartId` for an abandoned cart.
      * Returns a `HydrateResponse` object which can be used to populate the local cart
@@ -19,14 +19,13 @@ public class CartService {
     public func hydrateCart(cartId: String, completion: @escaping (Bool, HydrateResponse?, ErrorResponse?) -> Void) {
         //Build Route
         let route = CartRouter.hydrate(cartId) as URLRequestConvertible
-        
+
         //Call API
         Service<HydrateResponse, ErrorResponse>().request(route: route) { (result, responseObject, errorObject) in
             completion(result, responseObject, errorObject)
         }
     }
-    
-    
+
     /// Sends a request to the ordermentum service notifying it that the logged in user has added/removed items from their cart. This in-turn, allows for the abandoned cart functionality to work.
     /// - Parameters:
     ///   - requestObject: An object representing the actions taken on the cart for a given supplier/retailer/purchaser relationship
@@ -34,7 +33,7 @@ public class CartService {
     public func populateCart(_ requestObject: CartActionRequest, completion: @escaping (Bool, ErrorResponse?) -> Void) {
         //Build Route
         let route = CartRouter.populateCart(requestObject) as URLRequestConvertible
-        
+
         //Call API
         Service<EmptyDecodable, ErrorResponse>().request(route: route) { (result, _, errorObject) in
             completion(result, errorObject)
