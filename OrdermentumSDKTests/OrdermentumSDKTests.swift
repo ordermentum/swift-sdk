f8321dd41b94afeaf63125eacb9a3081ea8767f2
Brandon Stillitano <b.stillitano95@gmail.com>
2021-01-05 11:51:45 +1100

    Add test targets

---
+++ OrdermentumSDKTests/OrdermentumSDKTests.swift
@@ -0,0 +1,24 @@
+//
+//  OrdermentumSDKTests.swift
+//  OrdermentumSDKTests
+//
+//  Created by Brandon Stillitano on 5/1/21.
+//  Copyright © 2021 Ordermentum. All rights reserved.
+//
+
+import XCTest
+@testable import OrdermentumSDK
+
+class OrdermentumSDKTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+    }
+    
+    func testStringNotNil() {
+        XCTAssertNotNil("NotNilString")
+    }
+}
