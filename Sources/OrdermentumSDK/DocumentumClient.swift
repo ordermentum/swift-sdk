beac0222bea78e796d582beff68332e00cb440e6
Brandon Stillitano <b.stillitano95@gmail.com>
2021-05-27 15:27:47 +1000

    Update models

--- Sources/OrdermentumSDK/DocumentumClient.swift
+++ Sources/OrdermentumSDK/DocumentumClient.swift
@@ -10,7 +10,9 @@ import Alamofire
 
 public class DocumentumClient {
     //Data
-    public var baseURL: String = DocumentumClientURL.rootURL
+    public var baseURL: String = ""
+    public var token: String = ""
+    public var headers: [String: String] = [:]
     
     //Private Init to Stop Re-Initialisation
     private init() {}
@@ -19,23 +21,38 @@ public class DocumentumClient {
     //Services
     public var documentum: DocumentumService = DocumentumService()
     
-    public func urlRequest(path: String, method: HTTPMethod, parameters: Parameters, body: Codable?) throws ->  URLRequest {
+    // MARK: Convenience Methods
+    public func getHeaderToken() -> String {
+        return String(format: "Bearer \(token)")
+    }
+
+    public func urlRequest(path: String, method: HTTPMethod, parameters: Parameters, body: Codable?, timeout: Int = 10) throws -> URLRequest {
         //Setup Data
         let url = try baseURL.asURL()
-        let timeoutSeconds: Int = 10
-        
+        let timeoutSeconds: Int = timeout
+
         //Build Request
         var request = URLRequest(url: url.appendingPathComponent(path))
         request.httpMethod = method.rawValue
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.timeoutInterval = TimeInterval(timeoutSeconds * 1000)
         request.httpBody = body?.toJSONData()
-        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+
+        //Set Headers
+        for header in headers {
+            request.setValue(header.value, forHTTPHeaderField: header.key)
+        }
+
+        //Set Token
+        if !token.isEmpty {
+            request.setValue(getHeaderToken(), forHTTPHeaderField: "Authorization")
+        }
+
         //Set Conditional Body
         return try URLEncoding(destination: .queryString,
                                arrayEncoding: .brackets,
-                               boolEncoding: .literal).encode(request, with: parameters)
+                               boolEncoding: .literal).encode(request, with: parameters.filter { $0.value != nil })
     }
 }
 
